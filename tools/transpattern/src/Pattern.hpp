0cd8964c43d8ad2648be43818944f2f7b218b153
Xavier Mitault <xavier.mitault@epitech.eu>
2023-11-12 13:41:50 +0000

    fix(Pattern): compil issue with output command

--- tools/transpattern/src/Pattern.hpp
+++ tools/transpattern/src/Pattern.hpp
@@ -93,7 +93,7 @@ public:
     {
         std::string impl;
 
-        impl += "#include \"Pattern.hpp\"\n";
+        impl += "#include \"Pattern" + std::to_string(BOARD_SIZE) + ".hpp\"\n";
         impl += "\n";
         impl += "std::vector<Pattern" + std::to_string(BOARD_SIZE) + "> Pattern" + std::to_string(BOARD_SIZE) + "::getPatterns()\n";
         impl += "{\n";
