951bf2f50b4e655e0b8ecd50f10c4e0c09c2f2ea
Xavier Mitault <xavier.mitault@epitech.eu>
2023-11-14 12:12:07 +0000

    Merge remote-tracking branch 'origin/main' into 400Bits

---
+++ src/Perfcounter/Perfcounter.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <chrono>
+#include <cmath>
+#include <cstddef>
+#include <map>
+#include <unordered_map>
+
+class Perfcounter {
+public:
+    enum class PerfType : std::size_t {
+        BITSHIFT = 0,
+    };
+
+    class Counter {
+    public:
+        Counter(PerfType type);
+        ~Counter();
+
+    private:
+        std::chrono::high_resolution_clock::time_point _start;
+        PerfType _type;
+    };
+
+    class CounterStat {
+    public:
+        CounterStat();
+
+        void addCount(std::size_t count);
+        std::double_t getMeanCount() const;
+        std::size_t getMaxCount() const;
+        std::size_t getMinCount() const;
+
+    private:
+        std::size_t mean_count_add = 0;
+        std::size_t mean_count_num = 0;
+        std::size_t max_count = 0;
+        std::size_t min_count = 0;
+    };
+
+    static CounterStat &getCounter(PerfType type);
+
+    static void writeStats(const std::string &filename);
+
+private:
+    Perfcounter();
+
+    static std::unordered_map<PerfType, CounterStat> _counters;
+};
