5e99fdb2fddeb01a8478d8f43d137e4e53623f2a
Théophilus Homawoo <83962803+theophilushmwo@users.noreply.github.com>
2023-11-15 14:40:34 +0100

    feat: Used borders for move adjencency

--- src/Board/GomukuBoard.cpp
+++ src/Board/GomukuBoard.cpp
@@ -51,8 +51,8 @@ std::vector<std::pair<int, int>> GomukuBoard::getPossibleMoves() const {
         moves.emplace_back(10, 10);
         return moves;
     } else {
-        for (int x = 0; x < BOARD_SIZE; ++x) {
-            for (int y = 0; y < BOARD_SIZE; ++y) {
+        for (int x = minX; x <= maxX; ++x) {
+            for (int y = minY; y <= maxY; ++y) {
                 if (!isOccupied(x, y) && isAdjacentToOccupied(x, y)) {
                     moves.emplace_back(x, y);
                 }
